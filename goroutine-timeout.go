3f2b58548d0b4efe85641dde2b76ecdb4e533cce
daixiaoyun <daixiaoyun@baidu.com>
2021-01-08 10:01:05 +0800

    push to github

---
+++ goroutine-timeout.go
@@ -0,0 +1,23 @@
+package main
+
+import "time"
+
+func main() {
+	c := make(chan int)
+	o := make(chan bool)
+	go func() {
+		for {
+			select {
+			case v := <-c:
+				println(v)
+			case <-time.After(5 * time.Second):
+				println("timeout")
+				o <- true
+				break
+			}
+		}
+	}()
+
+	// 阻塞直到o中有值，也即超时
+	<-o
+}
